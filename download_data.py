961dd265311dd8ba80d41b9ecb0f805de1671c55
abdoul_rahim <zebaabdoulr@gmail.com>
2025-03-04 14:30:09 +0100

    Add data downloading and preprocessing scripts for credit card fraud detection

---
+++ download_data.py
@@ -0,0 +1,49 @@
+import os
+import hashlib
+import requests
+import pandas as pd
+import kagglehub
+import sklearn.model_selection as skms
+
+
+def get_dataset():
+    """Download the dataset from Kaggle."""
+    path = kagglehub.dataset_download("mlg-ulb/creditcardfraud")
+    file_path = path + "/creditcard.csv"
+    return file_path
+
+
+def get_train_test(file_path):
+    """Split the data into train and test sets."""
+    dataset = pd.read_csv(file_path)
+
+    # randomly sample 50% of the non-fraudulent transactions from
+    dataset = dataset.drop(dataset[dataset.Class == 0].sample(frac=0.5, random_state=42).index)
+
+    X, y = dataset.drop(columns=["Class"]), dataset["Class"]
+
+    # a stratified split is used to ensure that the class distribution is
+    # similar in both the training and testing sets
+    X_train, X_test, y_train, y_test = skms.train_test_split(
+        X, y, test_size=0.6, random_state=42, stratify=y
+    )
+
+    # combine the features and target into a single DataFrame
+    X_train["Class"] = y_train
+    X_test["Class"] = y_test
+
+    # then save the new train data to disk
+    X_train.to_csv('data/train.csv', index=False)
+    X_test.to_csv('data/test.csv', index=False)
+
+
+def download_data():
+    """Download the data from Kaggle and split it into train and test sets."""
+    file_path = get_dataset()
+    get_train_test(file_path)
+    # delete the downloaded file
+    os.remove(file_path)
+
+
+if __name__ == "__main__":
+    download_data()
---
+++ problem.py
@@ -0,0 +1,48 @@
+import os
+import pandas as pd
+import rampwf as rw
+from sklearn.model_selection import StratifiedShuffleSplit
+
+problem_title = 'Credit Card Fraud Detection - a binary classification problem'
+_target_column_name = 'Class'
+_ignore_column_names = []
+_prediction_label_names = [0, 1]
+
+Predictions = rw.prediction_types.make_binary(
+    label_names=_prediction_label_names
+)
+
+# An object implementing the workflow
+workflow = rw.workflows.Estimator()
+
+# The roc_auc score is the default metric for the challenge
+# because the classes are imbalanced
+score_types = [
+    rw.score_types.ROCAUC(name='roc_auc', precision=4),
+]
+
+
+def get_cv(X, y):
+    cv = StratifiedShuffleSplit(n_splits=8, test_size=0.2, random_state=42)
+    return cv.split(X, y)
+
+
+def csv_array_to_float(csv_array_string):
+    return list(map(float, csv_array_string[1:-1].split(',')))
+
+
+def _read_data(path, df_filename):
+    df = pd.read_csv(os.path.join(path, 'data', df_filename))
+    y_array = df[_target_column_name].values
+    X_dict = df.drop(_target_column_name, axis=1)
+    return X_dict, y_array
+
+
+def get_train_data(path='.'):
+    df_filename = 'train.csv'
+    return _read_data(path, df_filename)
+
+
+def get_test_data(path='.'):
+    df_filename = 'test.csv'
+    return _read_data(path, df_filename)
